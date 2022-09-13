8a86f4a8b16e002af973950f29047e8bfec7eae6
O-GODWIN <kosokoink@gmail.com>
2022-09-13 13:33:47 +0100

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,12 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+    print_alphabet();
+    return (0);
 }
