84b6a6941ad563aca8c554cf9811f52fecaef08a
O-GODWIN <kosokoink@gmail.com>
2022-09-13 16:55:42 +0100

    pit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,24 @@
-#include<main.h>
-using namespace std;
+#include <main.h>
 
-int main() {
+/**
+  * main - Prints the sum of all multiples of 3 or 5 up to 1024
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int i, z = 0;
 
-  int sum = 0;
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
 
-	for (int i = 3; i < 1000; i++) {
-
-		if (i%3==0 or i%5==0)
-
-			sum += i;
+		i++;
 	}
 
-	cout << sum;
-
-	return 0;
+	printf("%d\n", z);
+	return (0);
 }
