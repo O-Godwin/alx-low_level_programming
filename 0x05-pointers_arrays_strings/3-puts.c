f469f3453ecea92327a624e6e7fbd6448da64cb8
O-GODWIN <kosokoink@gmail.com>
2022-09-19 12:20:57 +0100

    commit all files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+
