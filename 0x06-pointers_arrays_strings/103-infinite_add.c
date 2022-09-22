7f83257ba349efbdb69ce123c1a29470ca2857c3
O-GODWIN <kosokoink@gmail.com>
2022-09-22 09:25:12 +0100

    add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,21 @@
 #include "main.h"
+
 /**
- *
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_number(int n)
 {
-	int index, n1_len = 0, n2_len = 0;
+	unsigned int num = n;
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
+	_putchar((num % 10) + '0');
 }
